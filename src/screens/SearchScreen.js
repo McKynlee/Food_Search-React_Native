450157dd9ded9df54a3e4e994ece4a40d98817df
McKynlee <mckynlee.westman@gmail.com>
2021-05-30 20:32:10 -0500

    added error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,20 +7,27 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    // Use baseURL you set up in yelp.js
-    const response = await yelp.get(`/search`, {
-      // 'params' as a second argument in the call will auto concatenate necessary params to the url
-      params: {
-        limit: 50,
-        term,
-        location: 'kansas city'
-      }
-    });
+    try {
 
-    setResults(response.data.businesses);
-  }
+      // Use baseURL you set up in yelp.js
+      const response = await yelp.get(`/search`, {
+        // 'params' as a second argument in the call will auto concatenate necessary params to the url
+        params: {
+          limit: 50,
+          term,
+          location: 'kansas city'
+        }
+      });
+
+      setResults(response.data.businesses);
+    } catch (err) {
+      console.log('ERROR:', err);
+      setErrorMessage('Sorry!  Something went wrong.')
+    }
+  };
 
   return <View>
     <SearchBar
@@ -28,6 +35,9 @@ const SearchScreen = () => {
       onTermChange={(newTerm) => setTerm(newTerm)}
       onTermSubmit={() => searchApi()}
     />
+
+    {errorMessage ? <Text>{errorMessage}</Text> : null}
+
     <Text>We have found {results.length} results</Text>
   </View>
 }
